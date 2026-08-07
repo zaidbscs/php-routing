11e2bdcefabeff9b5de6fadeb980c061385ad8f7
Zaid Hussain <137680240+zaidbscs@users.noreply.github.com>
2026-08-07 19:52:15 +0500

    Add navigation links for uploads and views

--- views/home.php
+++ views/home.php
@@ -10,6 +10,18 @@
     <nav>
         <a href="/home">Home</a> | 
         <a href="/contact">Contact Us</a>
+    <br>
+    Any Folder access is denied
+    <br>
+    <a href="/uploads">Uploads</a> 
+    <br>
+    <a href="/views">Views</a>
+    <br>
+    But files inside folder is acces via url <br>
+    <a href="/uploads/testing.php">uploads/testing.php</a> 
+    <br>
+    <a href="/views/contact.php">Contact Us</a>
+    
     </nav>
 </body>
 </html>
